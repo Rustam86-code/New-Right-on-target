41ada70e56047c5fe7482ceeba37ce16ba3594ea
Rustam86-code <rustamtl86@gmail.com>
2022-03-17 14:55:45 +0600

    Initial Commit

---
+++ New Right on target/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  New Right on target
+//
+//  Created by Rustam Tleubayev on 17.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
